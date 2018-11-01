ccd42ba89606535391f2343d494625ba149a8fed
Stephen Cassat <Stephencassat@yahoo.com>
2018-11-01 09:01:08 -0600

    Created CocoaView File in Swift

---
+++ DrawingProject/Resources/View/CreativityView.swift
@@ -0,0 +1,30 @@
+//
+//  CreativityView.swift
+//  DrawingProject
+//
+//  Created by Cassat, Stephen on 11/1/18.
+//  Copyright © 2018 Cassat Stephen. All rights reserved.
+//
+
+import UIKit
+
+public class CreativityView : UIView
+{
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    public override func draw(_ rect: CGRect) -> Void
+    {
+        // Drawing code
+    }
+    
+    private func createStickFigure() -> UIBezierPath
+    {
+        let figure : UIBezierPath = UIBezierPath()
+        
+        
+        return figure
+    }
+}
+
